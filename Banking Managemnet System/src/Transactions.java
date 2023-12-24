06758f5c92716a960ccf004b8e5b474dfa15dfc0
Yordanos Yshitla <yordiman614@gmail.com>
2023-12-24 16:51:38 +0300

    changes for different classes

--- Banking Managemnet System/src/Transactions.java
+++ Banking Managemnet System/src/Transactions.java
@@ -19,48 +19,40 @@ public class Transactions extends JFrame implements ActionListener{
         // Create a label with the image and set its bounds, and add to the frame
         JLabel image = new JLabel(i3);
         image.setBounds(0, 0, 900, 900);
-
-        // Add the label to the frame
         add(image);
 
-        // Create a label with the text "please select your transaction" and set its properties
         JLabel text = new JLabel("Please Select Your Transaction");
         text.setBounds(210, 310, 700, 35);
         text.setForeground(Color.WHITE);
         text.setFont(new Font("System", Font.BOLD, 16));
         image.add(text);
 
-        // Create a button with the text "Deposit" and set its bounds and action listener
+        // Create a buttons and setting their bounds and action listener
         deposit = new JButton("Deposit");
         deposit.setBounds(175, 420, 140, 30);
         deposit.addActionListener(this);
         image.add(deposit);
 
-        // Create a button with the text "Cash Withdrow" and set its bounds and action listener
         withdraw = new JButton("Cash Withdrow");
         withdraw.setBounds(360, 420, 140, 30);
         withdraw.addActionListener(this);
         image.add(withdraw);
 
-        // Create a button with the text "Fast Cash" and set its bounds and action listener
         fastCash = new JButton("Fast Cash");
         fastCash.setBounds(175, 455, 140, 30);
         fastCash.addActionListener(this);
         image.add(fastCash);
 
-        // Create a button with the text "Pin Change" and set its bounds and action listener
         pinChange = new JButton("Pin Change");
         pinChange.setBounds(175, 490, 140, 30);
         pinChange.addActionListener(this);
         image.add(pinChange);
 
-        // Create a button with the text "Balance Enquiry" and set its bounds and action listener
         balanceEnquiry = new JButton("Balance Enquiry");
         balanceEnquiry.setBounds(360, 455, 140, 30);
         balanceEnquiry.addActionListener(this);
         image.add(balanceEnquiry);
 
-        // Create a button with the text "Exit" and set its bounds and action listener
         exit = new JButton("Exit");
         exit.setBounds(360, 525, 140, 27);
         exit.addActionListener(this);
