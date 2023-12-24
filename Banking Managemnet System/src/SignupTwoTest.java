b7f3e89913f6730fd1724c11fcc6050e7efe5a04
yosef <josefdagne5@gmial.com>
2023-12-24 10:03:58 +0300

    adding a unit test for SignupTwo class

---
+++ Banking Managemnet System/src/SignupTwoTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import java.awt.event.ActionEvent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SignupTwoTest {
+@Test
+public void testActionPerformed(){
+        // Manually set values for the JComboBox components (religion, category, income, education, occupation)
+        SignupTwo signupTwo=new SignupTwo("");
+        signupTwo.religion.setSelectedItem("Muslim");
+        signupTwo.Catagory.setSelectedItem("OBC");
+        signupTwo.income.setSelectedItem("<50000");
+        signupTwo.education.setSelectedItem("Under Graduate");
+        signupTwo.Occupation.setSelectedItem("Student");
+        // Manually set values for radio buttons and text fields
+        signupTwo.syes.setSelected(true);
+        signupTwo.sno.setSelected(false);
+        signupTwo.eyes.setSelected(true);
+        signupTwo.eno.setSelected(false);
+        signupTwo.pan.setText("AB1234567");
+        signupTwo.phon.setText("1234567890");
+
+        signupTwo.actionPerformed(null);
+        assertFalse(signupTwo.isVisible());
+
+
+    }
+@Test
+public void testActionPerformedNextShouldTransferToSignUpThree(){
+        SignupTwo signupTwo=new SignupTwo("");
+        signupTwo.next.setSelected(true);
+        signupTwo.actionPerformed(new ActionEvent(signupTwo.next, ActionEvent.ACTION_PERFORMED, "next"));
+       // assertFalse(signupTwo.isVisible());
+
+}
+}
