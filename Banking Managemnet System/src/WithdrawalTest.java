57c35ee21fa2b3c902b47347841c781690101482
Hawa4656 <hawomoh@gmail.com>
2023-12-24 15:30:55 +0300

    comments on the WithdrawalTest class

--- Banking Managemnet System/src/WithdrawalTest.java
+++ Banking Managemnet System/src/WithdrawalTest.java
@@ -1,14 +1,16 @@
+// Importing some internal and external libraries that are used for unit test
 import org.junit.Test;
 import java.awt.event.ActionEvent;
-import static junit.framework.Assert.assertFalse;
 
 public class WithdrawalTest {
+    // This test is used to test back button of the withdrawal class
     @Test
     public void testActionPerformedNextShouldTransferToWithdrawal(){
         Withdrawal witdraw = new Withdrawal("");
         witdraw.back.setSelected(true);
         witdraw.actionPerformed(new ActionEvent(witdraw.back, ActionEvent.ACTION_PERFORMED, "next"));
     }
+    // And this one test the withdraw button
     @Test
     public void testActionPerformedNextShouldTransferToWithdrawalToTransaction(){
         Withdrawal witdraw1 = new Withdrawal("");
